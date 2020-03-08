ef1e760b49ecd78f87893a57279f05d54db2b2fd
tommypeps <nosoycomplicao@gmail.com>
2020-03-08 13:50:26 +0100

    ini

---
+++ README.md
@@ -0,0 +1,94 @@
+# Simply setup mac script
+
+My humble setup script:
+
+##Install
+
+
+Download the script
+
+```
+curl -sL https://raw.githubusercontent.com/tommypeps/XXXXXnameRepository/master/installScript.sh
+```
+
+Review the script:
+
+```
+less installScript.sh
+```
+
+Install the script:
+
+```
+chmod +x install.sh
+./install.sh
+```
+
+##Utils
+
+* [Zsh](https://github.com/ohmyzsh/ohmyzsh)
+* [rvm](https://rvm.io)
+* [nvm]([https://github.com/nvm-sh/nvm](https://github.com/nvm-sh/nvm))
+* [brew](https://brew.sh) 
+* [Z Script](https://github.com/rupa/z)
+* [nmap](https://github.com/nmap/nmap)
+* [tree](https://github.com/kddeisz/tree)
+
+##Alias
+
+`alias lt='ls -lGt'`
+
+`alias ll='ls -lG'`
+
+`alias la='ls -la'`
+
+`alias ip='curl ipecho.net/plain; echo'`
+
+`alias prettyJson='python -m json.tool'`
+
+`alias h="history"`
+
+`alias c="clear"`
+
+##Apps
+* [Postman](https://www.postman.com)
+* [Sourcetree](https://www.sourcetreeapp.com)
+* [Sublime-text](https://www.sublimetext.com)
+* [Fastlane](https://fastlane.tools)
+* [Slack](https://slack.com/)
+* [Firefox](https://www.mozilla.org/)
+* [Opera](https://www.opera.com/)
+* [Skype](https://www.skype.com/)
+* [Clipy](https://github.com/Clipy/Clipy)
+* [Proxyman](https://proxyman.io/)
+* [Xrg](https://github.com/mikepj/XRG)
+* [Keka](https://www.keka.io/)
+* [Overkill](https://github.com/KrauseFx/overkill-for-mac)
+* [Visual-studio-code](https://code.visualstudio.com/?wt.mc_id=DX_841432)
+* [Microsoft-teams](https://teams.microsoft.com/downloads)
+* [Zoom](https://zoom.us)
+* [Captain](https://github.com/jenssegers/captain)
+* [Dockers](https://www.docker.com)
+* [Macdown](https://macdown.uranusjr.com)
+* [Sequel-pro](https://www.sequelpro.com)
+* [betterzip](https://macitbetter.com)
+
+##Plugins
+
+* [quicklook-json](https://github.com/sindresorhus/quick-look-plugins)
+* [provisionql](https://github.com/ealeksandrov/ProvisionQL)
+
+##Other apps
+* [Jdownloader](https://jdownloader.org)
+* [Virtualbox](https://www.virtualbox.org)
+* [Spotify](https://www.spotify.com)
+* [Transmission](https://transmissionbt.com)
+* [HandBrake](https://handbrake.fr)
+
+
+##References:
+
+* [Laptop](https://github.com/thoughtbot/laptop)
+* [Dev-setup](https://github.com/donnemartin/dev-setup)
+* [mac-setup-script](https://github.com/pathikrit/mac-setup-script)
+
---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "Install Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Install rvm"
+\curl -sSL https://get.rvm.io | bash -s stable
+echo "install nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
+
+echo "Install brew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Install Z script..."
+curl -o ~/scritps/z.sh --create-dirs https://raw.githubusercontent.com/rupa/z/master/z.sh | bash
+chmod +x ~/scritps/z.sh
+echo "source ~/scritps/z.sh" >> ~/.zshrc
+>> ~/.zshrc
+
+echo "Install utilitis ..."
+brew install nmap
+brew install tree
+brew install wget
+brew cask install gswitch
+
+echo "Install apps software ..."
+brew cask install postman
+brew cask install sourcetree
+brew cask install sublime-text
+brew cask install Opera
+brew cask install fastlane
+brew cask install slack
+brew cask install firefox
+brew cask install Opera
+brew cask install skype
+brew cask install clipy
+brew cask install proxyman
+brew cask install xrg
+brew cask install keka
+brew cask install overkill
+brew cask install visual-studio-code
+brew cask install microsoft-teams
+brew cask install zoom
+brew cask install captain
+brew cask install dockers
+brew cask install macdown
+brew cask install sequel-pro
+
+echo "Install plugins ..."
+brew cask install betterzip
+brew cask install quicklook-json
+brew cask install provisionql
+
+echo "Install other apps ..."
+brew cask install jdownloader
+brew cask install virtualbox
+brew cask install spotify 
+brew cask install transmission
+brew cask install HandBrake
+
+echo "Clean"
+brew cleanup
+brew cask cleanup
+
+echo "add alias ..."
+
+echo "
+alias lt='ls -lGt'
+alias ll='ls -lG'
+alias la='ls -la'
+alias ip='curl ipecho.net/plain; echo'
+alias prettyJson='python -m json.tool'
+alias h='history'
+alias c='clear'
+" >> ~/.zshrc
+
+curl -o ~/scritps/z.sh --create-dirs https://raw.githubusercontent.com/rupa/z/master/z.sh | bash
+chmod +x ~/scritps/z.sh 
+echo "source ~/scritps/z.sh" >> ~/.zshrc
+
+echo "Done!"
